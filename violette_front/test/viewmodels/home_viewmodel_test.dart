9149d6ce354693bfb3f52dbe9ab99fd26084c3c0
Quentin CAVIN <quentin.cavin@gmail.com>
2026-04-19 20:08:08 +0200

    fix(test): supprimer l'import firebase_auth redondant
    - Corrige flutter analyze (unnecessary_import) pour la CI

--- violette_front/test/viewmodels/home_viewmodel_test.dart
+++ violette_front/test/viewmodels/home_viewmodel_test.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:violette_front/app/app.locator.dart';
