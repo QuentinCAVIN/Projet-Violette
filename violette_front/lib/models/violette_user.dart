48a42c10ec09f15f1ea128996609edd65cc9dc50
Quentin CAVIN <quentin.cavin@gmail.com>
2026-04-25 14:31:03 +0200

    refactor(front): supprimer les reliquats Firestore du frontend

--- violette_front/lib/models/violette_user.dart
+++ violette_front/lib/models/violette_user.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'enums/role.dart';
 
 class VioletteUser {
@@ -16,35 +14,6 @@ class VioletteUser {
     required this.email,
     required this.roles,
   });
-
-//****************************************************************************//
-//Fonctions Mapping Firestore                                                 //
-//****************************************************************************//
-  Map<String, dynamic> toFirestore() {
-    return {
-      "firstName": firstName,
-      "lastName": lastName,
-      "email": email,
-      "roles": roles.map((role) => role.name).toList(),
-    };
-  }
-
-  // TODO REFACTO à faire
-  factory VioletteUser.fromFirestore(
-    DocumentSnapshot<Map<String, dynamic>> snapshot,
-    SnapshotOptions? options,
-  ) {
-    final data = snapshot.data();
-    return VioletteUser(
-      uid: snapshot.id,
-      firstName: data!['firstName'],
-      lastName: data['lastName'],
-      email: data['email'],
-      roles: (data['roles'] as Iterable<dynamic>)
-          .map((e) => roleFromString(e.toString()))
-          .toList(), // François
-    );
-  }
 }
 // TODO Poser la question a ELies sur l'emplacement de l'enum ROle qui ne sert que pour User
 // Felix -> Ne pas placer dans User pour faciliter la testabilité en appellant User.getRole == Role.mescouilles
