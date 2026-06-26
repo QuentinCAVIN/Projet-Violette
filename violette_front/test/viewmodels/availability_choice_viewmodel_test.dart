650c11e94e74d0536f3e4aad847eaa59c3a5d4b6
Quentin CAVIN <quentin.cavin@gmail.com>
2026-06-26 17:50:40 +0200

    test(availability): supprimer les imports inutilisés du test ViewModel
    - Corriger l'échec de flutter analyze en CI

--- violette_front/test/viewmodels/availability_choice_viewmodel_test.dart
+++ violette_front/test/viewmodels/availability_choice_viewmodel_test.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:dio/dio.dart';
 import 'package:violette_front/app/app.locator.dart';
